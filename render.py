289ceef0e5980b01da8cc5a7a6de1b267b4459c1
Sharon Lee <sharonal@stanford.edu>
2024-09-14 18:33:30 -0700

    render

--- render.py
+++ render.py
@@ -14,7 +14,7 @@ import matplotlib as mpl
 import matplotlib.cm as cm
 import imageio
 import numpy as np
-
+import cv2
 
 def render_set(model_path, name, iteration, views, gaussians, pipeline, background):
     render_path = os.path.join(model_path, name, "ours_{}".format(iteration), "renders")
@@ -22,6 +22,13 @@ def render_set(model_path, name, iteration, views, gaussians, pipeline, backgrou
 
     makedirs(render_path, exist_ok=True)
     makedirs(gts_path, exist_ok=True)
+
+    sample = render(views[0], gaussians, pipeline, background)
+    fps = 10
+    height, width, _ = sample["depth"].shape
+    depth_video = cv2.VideoWriter(os.path.join("videos", f"{os.path.basename(render_path)}_depth.mp4"), cv2.VideoWriter_fourcc(*'mp4v'), fps, (width, height))
+    height, width, _ = sample["render"].shape
+    rgb_video = cv2.VideoWriter(os.path.join("videos", f"{os.path.basename(render_path)}_rgb.mp4"), cv2.VideoWriter_fourcc(*'mp4v'), fps, (width, height))
 
     for idx, view in enumerate(tqdm(views, desc="Rendering progress")):
         rendering = render(view, gaussians, pipeline, background)
@@ -36,11 +43,18 @@ def render_set(model_path, name, iteration, views, gaussians, pipeline, backgrou
         
         inferno_mapper = cm.ScalarMappable(norm=normalizer,cmap="inferno")
         colormap_inferno = (inferno_mapper.to_rgba(render_depth.cpu().numpy())*255).astype('uint8') 
-        imageio.imwrite(os.path.join(render_path, '{0:05d}'.format(idx) + "_depth_inferno.png"), colormap_inferno)
+        depth_video.write(cv2.imread(colormap_inferno))
+        rgb_video.write(cv2.imread(rendered_image))
+    cv2.destroyAllWindows()
+    depth_video.release()
+    rgb_video.release()
+        # imageio.imwrite(os.path.join(render_path, '{0:05d}'.format(idx) + "_depth_inferno.png"), colormap_inferno)
         
-        torchvision.utils.save_image(rendered_image, os.path.join(render_path, '{0:05d}'.format(idx) + ".png"))
-        torchvision.utils.save_image(rendered_depth, os.path.join(render_path, '{0:05d}'.format(idx) + "_depth.png"))
+        # torchvision.utils.save_image(rendered_image, os.path.join(render_path, '{0:05d}'.format(idx) + ".png"))
+        # torchvision.utils.save_image(rendered_depth, os.path.join(render_path, '{0:05d}'.format(idx) + "_depth.png"))
         #torchvision.utils.save_image(gt, os.path.join(gts_path, '{0:05d}'.format(idx) + ".png"))
+
+        
 
 def render_sets(dataset : ModelParams, iteration : int, pipeline : PipelineParams, skip_train : bool, skip_test : bool, args_dict):
     with torch.no_grad():
@@ -75,7 +89,7 @@ if __name__ == "__main__":
     args = get_combined_args(parser)
     print("Rendering " + args.model_path)
 
-    args.render_only = True
+    args.render_only = True # NB render_only puts everything in training set
     
     safe_state(args.quiet)
 
