4621b65d29580e46395eb7aaf4790e97fb566d3a
Sharon Lee <sharonal@stanford.edu>
2024-10-06 15:49:57 -0700

    hacky

--- scene/dataset_readers.py
+++ scene/dataset_readers.py
@@ -106,6 +106,7 @@ def fetchPly(path):
     vertices = plydata['vertex']
     positions = np.vstack([vertices['x'], vertices['y'], vertices['z']]).T
     colors = np.vstack([vertices['red'], vertices['green'], vertices['blue']]).T / 255.0
+    colors = np.zeros_like(colors)
     normals = np.vstack([vertices['nx'], vertices['ny'], vertices['nz']]).T
     return BasicPointCloud(points=positions, colors=colors, normals=normals)
 
