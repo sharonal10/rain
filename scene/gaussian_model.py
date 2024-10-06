56609a32f731610a325d1e3d51a7f75ec51f1e0d
Sharon Lee <sharonal@stanford.edu>
2024-10-06 02:11:00 -0700

    debug

--- scene/gaussian_model.py
+++ scene/gaussian_model.py
@@ -441,5 +441,6 @@ class GaussianModel:
         torch.cuda.empty_cache()
 
     def add_densification_stats(self, viewspace_point_tensor, update_filter):
+        import pdb; pdb.set_trace()
         self.xyz_gradient_accum[update_filter] += torch.norm(viewspace_point_tensor.grad[update_filter,:2], dim=-1, keepdim=True)
         self.denom[update_filter] += 1
