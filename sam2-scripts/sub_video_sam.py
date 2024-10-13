37b984a3df535a888bf91e6f4bdbafcccfa4aa70
Sharon Lee <sharonal@stanford.edu>
2024-10-13 13:27:42 -0700

    save first photo

--- sam2-scripts/sub_video_sam.py
+++ sam2-scripts/sub_video_sam.py
@@ -8,6 +8,28 @@ import matplotlib.pyplot as plt
 import argparse
 import cv2
 
+def show_anns(anns, borders=True):
+    if len(anns) == 0:
+        return None
+    sorted_anns = sorted(anns, key=(lambda x: x['area']), reverse=True)
+    ax = plt.gca()
+    ax.set_autoscale_on(False)
+
+    img = np.ones((sorted_anns[0]['segmentation'].shape[0], sorted_anns[0]['segmentation'].shape[1], 4))
+    img[:, :, 3] = 0
+    for ann in sorted_anns:
+        m = ann['segmentation']
+        color_mask = np.concatenate([np.random.random(3), [0.5]])
+        img[m] = color_mask
+        if borders:
+            import cv2
+            contours, _ = cv2.findContours(m.astype(np.uint8), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+            # Try to smooth contours
+            contours = [cv2.approxPolyDP(contour, epsilon=0.01, closed=True) for contour in contours]
+            cv2.drawContours(img, contours, -1, (0, 0, 1, 0.4), thickness=1)
+
+    ax.imshow(img)
+    return img
 
 """
 input directory structure should be as follows:
@@ -117,6 +139,15 @@ mask_generator = SAM2AutomaticMaskGenerator(
 )
 auto_masks = mask_generator.generate(first_frame)
 print("Number of auto-masks:", len(auto_masks))
+
+plt.figure(figsize=(20, 20))
+new_img = show_anns(auto_masks)
+plt.axis('off')
+
+if new_img is not None:
+    output_path = os.path.join(binary_mask_output_dir, 'first.jpg')
+    plt.imsave(output_path, new_img)
+plt.close()
 
 # Add every 'auto-mask' as it's own prompt for video tracking
 predictor = build_sam2_video_predictor(model_cfg, sam2_checkpoint, device=device)
