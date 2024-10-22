bf053b80d4331c51a76f267985565d885d5b5488
Sharon Lee <sharonal@stanford.edu>
2024-10-22 12:34:53 -0700

    try disallowing all to be pruned

--- scene/gaussian_model.py
+++ scene/gaussian_model.py
@@ -434,7 +434,7 @@ class GaussianModel:
             big_points_vs = self.max_radii2D > max_screen_size
             big_points_ws = self.get_scaling.max(dim=1).values > 0.1 * extent
             prune_mask = torch.logical_or(torch.logical_or(prune_mask, big_points_vs), big_points_ws)
-        if not torch.all(self.get_opacity < min_opacity) :
+        if not torch.all(self.get_opacity < min_opacity):
             self.prune_points(prune_mask)
 
         torch.cuda.empty_cache()
