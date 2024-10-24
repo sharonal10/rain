4596ae4aec6ce986471ea0e4628a35c8fe1c626e
Sharon Lee <sharonal@stanford.edu>
2024-10-23 23:50:16 -0700

    rendering info

--- scene/gaussian_model.py
+++ scene/gaussian_model.py
@@ -217,7 +217,7 @@ class GaussianModel:
 
         extra_f_names = [p.name for p in plydata.elements[0].properties if p.name.startswith("f_rest_")]
         extra_f_names = sorted(extra_f_names, key = lambda x: int(x.split('_')[-1]))
-        assert len(extra_f_names)==3*(self.max_sh_degree + 1) ** 2 - 3
+        assert len(extra_f_names)==3*(self.max_sh_degree + 1) ** 2 - 3, (len(extra_f_names), self.max_sh_degree, 3*(self.max_sh_degree + 1) ** 2 - 3)
         features_extra = np.zeros((xyz.shape[0], len(extra_f_names)))
         for idx, attr_name in enumerate(extra_f_names):
             features_extra[:, idx] = np.asarray(plydata.elements[0][attr_name])
