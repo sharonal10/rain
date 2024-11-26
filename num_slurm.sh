b7cbe39b9c3d27781800e206e40fb4c0a400c51b
Sharon Lee <sharonal@stanford.edu>
2024-11-26 15:58:48 -0800

    train but render

--- num_slurm.sh
+++ num_slurm.sh
@@ -6,4 +6,8 @@ python -m tu.sbatch.sbatch_sweep --time 24:00:00 \
 
     python -m tu.sbatch.sbatch_sweep --time 24:00:00 \
     --proj_dir /viscam/projects/image2Blender/RAIN-GS --conda_env rain \
-    --job "11_25_sanity_check_synth2_chair" --command "python train_assembly_single.py -s sugar/imgs/11_20_synth2_chair/ --exp_name 11_25_sanity_check_synth2_chair --eval --box_gen --box_name box --ours_new --num_cams 300 --save_iterations 1 1000 3500 7000 --iterations 7000 --input_pcs output/11_20_phase_1_synth2_chair/point_cloud_0/iteration_7000/point_cloud.ply" --partition viscam --account viscam --gpu_type a6000 --cpus_per_task 8 --num_gpus 1 --mem 64G
+    --job "11_25_sanity_check_synth2_chair" --command "python train_assembly_single.py -s sugar/imgs/11_20_synth2_chair/ --exp_name 11_25_sanity_check_synth2_chair2 --eval --box_gen --box_name box --ours_new --num_cams 300 --save_iterations 1 1000 3500 7000 --iterations 7000 --input_pcs output/11_20_phase_1_synth2_chair/point_cloud_0/iteration_7000/point_cloud.ply" --partition viscam --account viscam --gpu_type a6000 --cpus_per_task 8 --num_gpus 1 --mem 64G
+
+    python -m tu.sbatch.sbatch_sweep --time 24:00:00 \
+    --proj_dir /viscam/projects/image2Blender/RAIN-GS --conda_env rain \
+    --job "11_24_train_but_render" --command "python train_but_render.py -s sugar/imgs/11_20_synth2/ --exp_name 11_26_train_but_render --eval --box_gen --box_name box --ours_new --num_cams 300 --save_iterations 1 1000 3500 7000 --iterations 7000 --input_pcs output/11_17-phase_1_scene_table/point_cloud_0/iteration_7000/point_cloud.ply output/11_20_phase_1_synth2_chair/point_cloud_0/iteration_7000/point_cloud.ply" --partition viscam --account viscam --gpu_type a6000 --cpus_per_task 8 --num_gpus 1 --mem 64G
